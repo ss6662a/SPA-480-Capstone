8ebe27356c53a4d6902d0364d679595cebb43705
ss6662a <ss6662a@american.edu>
2025-03-27 18:18:51 -0400

    synth run

--- Code/uk-synth-test.R
+++ Code/uk-synth-test.R
@@ -2,8 +2,8 @@
 # failing to do...
 
 
-control_countries <- c("Ireland", "Sweden", "Norway", "United States", "Poland", 
-                        "Italy", "New Zealand", "France", "Canada", "Brazil", "South Korea")
+control_countries <- c("Ireland", "Sweden", "Norway", "Poland", 
+                        "Italy", "New Zealand", "Canada", "Brazil")
 
 
 
@@ -14,9 +14,14 @@ uk_treat <- full_data %>%
   mutate(
    country_id =  as.numeric(as.factor(country)),
    .after = country
-  ) %>% 
+  ) %>%
+  mutate(country = as.character(country)) %>% 
   filter(country %in% control_countries | country == "United Kingdom") %>% 
-  
+  filter(year >= 2006 & year <= 2018)
+uk_treat <- as.data.frame(uk_treat)
+
+
+colSums(is.na(uk_treat))
   
   
 control_country_ids <- uk_treat %>% 
@@ -24,20 +29,17 @@ control_country_ids <- uk_treat %>%
   distinct(country_id) %>% 
   as.vector()
 
-# filter out NA values
 
 uk_prepped_data <- dataprep(
   foo = uk_treat,
   
-  predictors = c(
-    "homogeneity", 
-    "gdp", 
-    "regime_length", 
-    "ethnicity_index"),
+  predictors = c("n_rai", "n_sharedrule", "n_selfrule"),
   
   predictors.op = "mean",
   
   special.predictors = list(
+    list("gdp", 2006:2018, "mean"),
+    list("years_since_regime_change", 2006:2018, "mean"),
     list("ethnicity_index", 2006:2013, "mean")
   ),
   
@@ -50,11 +52,14 @@ uk_prepped_data <- dataprep(
   
   treatment.identifier = 179,
   
-  controls.identifier = c(24, 32, 62, 82, 85, 122, 128, 137, 157, 164, 180),
+  controls.identifier = c(24, 32, 82, 85, 122, 128, 137, 164),
   
   time.predictors.prior = c(2006:2011),
 
   time.optimize.ssr = c(2006:2011),  # Pre-treatment years used for optimization
   time.plot = c(2006:2018)  # Full period for visualization
 )
-colSums(is.na(uk_treat))
+
+uk_synth <- synth(uk_prepped_data)
+
+
